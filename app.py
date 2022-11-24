c3a85cd5804b0dc296f8dba8b0e1c91dadc2b9f6
Jiho-Hong <hongzoh@gmail.com>
2022-11-24 16:13:04 +0900

    web crawling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import google_keyword
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -6,19 +7,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))  # 접속하는 url
 def index():
-    if request.method == "POST":
-        # user=request.form['user'] # 전달받은 name이 user인 데이터
-        print(request.form.get('user'))  # 안전하게 가져오려면 get
-        user = request.form.get('user')
-        data = {'level': 60, 'point': 360, 'exp': 45000}
-        return render_template('index.html', user=user, data=data)
-    elif request.method == "GET":
-        user = "튜토리얼"
-        data = {'level': 60, 'point': 360, 'exp': 45000}
-        return render_template('index.html', user=user, data=data)
+    # 웹 페이지에서 name="xxx"인 요소의 value 가져오기
+    print(request.form.get('keyword1'))
+    print(request.form.get('keyword2'))
+    keyword1 = request.form.get('keyword1')
+    keyword2 = request.form.get('keyword2')
+
+    # 위의 값이 있을 때만 크롤링 검색 결과 반환
+    if keyword1 is not None and keyword2 is not None:
+        data = {
+            keyword1: google_keyword.get_search_count(keyword1).get('number'),
+            keyword2: google_keyword.get_search_count(keyword2).get('number'),
+        }
+        return render_template('index.html', data=data)
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # host 등을 직접 지정하고 싶다면
-    # app.run(host="127.0.0.1", port="5000", debug=True)
