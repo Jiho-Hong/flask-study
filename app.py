670e7988037f4937e2ba0f9e7299c8dce6f70e86
Jiho-Hong <hongzoh@gmail.com>
2022-11-24 15:20:31 +0900

    send data from server to client

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')  # 접속하는 url
 def index():
-  return render_template('index.html')
+  return render_template('index.html', user="튜토리얼", data={'level': 60, 'point': 360, 'exp': 45000})
 
 
 if __name__ == "__main__":
